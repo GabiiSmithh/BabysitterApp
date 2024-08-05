23aa0fe434ffb280800be295423dadae752dfe8f
João Otavio <joao.korczovei@luizalabs.com>
2024-08-05 09:18:27 -0300

    feat: create babysitter handler

---
+++ server/internal/handler/express/babysitter.js
@@ -0,0 +1,74 @@
+export class ExpressBabysitterHandler {
+    constructor({ babysitterService }) {
+        this.babysitterService = babysitterService;
+    }
+
+    async getByID(req, res) {
+        try {
+            const babysitter = await this.babysitterService.getByID(req.params.user_id);
+
+            if (!babysitter) {
+                return res.status(404).json({ message: 'Babysitter not found.' });
+            }
+
+            return res.status(200).json(babysitter);
+        } catch (error) {
+            // TODO: return correct status for different cases
+            return res.status(400).json({ message: error.message });
+        }
+    }
+
+    async list(_, res) {
+        try {
+            const babysitters = await this.babysitterService.list();
+
+            return res.status(200).json(babysitters);
+        } catch (error) {
+            // TODO: return correct status for different cases
+            return res.status(400).json({ message: error.message });
+        }
+    }
+
+    async create(req, res) {
+        try {
+            const createBabysitterDTO = {
+                name: req.body.name,
+                gender: req.body.gender,
+                email: req.body.email,
+                password: req.body.password,
+                cellphone: req.body.cellphone,
+                birthDate: req.body.birth_date,
+                experienceMonths: req.body.experience_months
+            };
+
+            const createdBabysitter = await this.babysitterService.create(createBabysitterDTO);
+
+            delete createdBabysitter.password
+
+            return res.status(201).json(createdBabysitter);
+        } catch (error) {
+            // TODO: return correct status for different cases
+            return res.status(400).json({ message: error.message });
+        }
+    }
+
+    async update(req, res) {
+        try {
+            const updateBabysitterDTO = {
+                id: req.params.user_id,
+                name: req.body.name,
+                email: req.body.email,
+                cellphone: req.body.cellphone,
+                experienceMonths: req.body.experience_months,
+                rating: req.body.rating,
+            };
+
+            const updatedBabysitter = await this.babysitterService.update(updateBabysitterDTO);
+
+            return res.status(200).json(updatedBabysitter);
+        } catch (error) {
+            // TODO: return correct status for different cases
+            return res.status(400).json({ message: error.message });
+        }
+    }
+}
